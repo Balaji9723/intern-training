d68091649e63f21fe650325bb4da809bbc3dc739
BALAJITG <balajithasma6@gmail.com>
2026-06-12 16:20:56 +0530

    Add functions, lists, and dictionaries

--- day_4/sample.py
+++ day_4/sample.py
@@ -32,16 +32,56 @@
 
 # guessing game - for loop
 
-result = 20
-for i in range (1,3):
-    guess = int(input("Enter your guess: "))
+# result = 20
+# for i in range (1,3):
+#     guess = int(input("Enter your guess: "))
 
-    if guess ==result:
-        print(" You guessed correctly")
-        break
+#     if guess ==result:
+#         print(" You guessed correctly")
+#         break
 
-    elif guess < result:
-        print("Too Low")
+#     elif guess < result:
+#         print("Too Low")
 
-    else:
-        print("Too High")
+#     else:
+#         print("Too High")
+
+
+# rows= 5
+# for i in range(rows):
+#     for j in range(rows):
+#         print("*", end=" ")
+#     print()     
+
+# patterns programs..., (*)   
+
+# n= int (input("enter the number:"))
+# for i in range (n):
+#     for j in range (n-i):
+#         print("*",end =" ")
+#     print()    
+
+# n= int (input("enter the number:"))
+# for i in range (n-1):
+#     for j in range (n):
+#         print("*",end =" ")
+#     print()    
+
+# n= int (input("enter the number:"))
+# for i in range (n):
+#     for j in range (n-i-1):
+#         print(" ",end =" ")
+
+#     for j in range(i+1):
+#         print("*", end=" ")   
+#     print()
+
+
+n= int (input("enter the number:"))
+for i in range (n):
+    for j in range (i):
+        print(" ",end =" ")
+
+    for j in range(n-i):
+        print("*", end=" ")   
+    print()
